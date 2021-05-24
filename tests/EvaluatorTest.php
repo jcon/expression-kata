3aa3fd2b8b049e18a9ba5986a96c23232d00a134
Jim Connell <jconnell@etsy.com>
2021-05-24 17:04:07 -0400

    Switch to table driven tests.

--- tests/EvaluatorTest.php
+++ tests/EvaluatorTest.php
@@ -3,106 +3,64 @@ use PHPUnit\Framework\TestCase;
 
 final class EvaluatorTest extends TestCase
 {   
-    public function testEmptyStringReturnsZero(): void
-    {           
+    /**
+     * @dataProvider validTokensProvider
+     */
+    public function testValidTokenizerInput($expr, $expected) {
         $this->assertEquals(
-            "0", 
-            Evaluator::evaluate('')
-        );
-    }           
-        
-    public function testPlainIntReturnsThatInt(): void
-    {
-        $this->assertEquals(
-            "42",
-            Evaluator::evaluate('42')
+            $expected,
+            Evaluator::tokenize($expr)
         );
     }
 
-    public function testSimpleAdditionReturnsResult(): void
-    {
+    public function validTokensProvider() {
+        return [
+            /** [input, expected] */
+            ['', []],
+            ['42', ['42']],
+            ['8 + 2', ['8', '+', '2']]
+        ];
+    }
+
+    /**
+     * @dataProvider validExpressionProvider
+     */
+    public function testValidExpressions($expr, $expected) {
         $this->assertEquals(
-            "10",
-            Evaluator::evaluate('8 + 2')
+            $expected,
+            Evaluator::evaluate($expr)
         );
     }
 
-    public function testTokenizeEmptyStringReturnsEmptyArray(): void
-    {           
-        $this->assertEquals(
-            [], 
-            Evaluator::tokenize('')
-        );
-    }   
-
-    public function testTokenizeSingleIntReturnsInt(): void
-    {           
-        $this->assertEquals(
-            ['18'], 
-            Evaluator::tokenize('18')
-        );
+    public function validExpressionProvider() {
+        return [
+            /** [input, expected] */
+            ['', '0'],
+            ['42', '42'],
+            ['8 + 2', '10'],
+            ['8 - 2', '6'],
+            ['8 - 2 + 5', '11'],
+            ['15 * 3', '45'],
+            ['15 * 3 * 3', '135'],
+            ['100 / 3', '33']
+        ];
     }
 
-    public function testTokenizeExpressionReturnsExpressionTokens(): void
-    {           
-        $this->assertEquals(
-            ['18', '+', '5'], 
-            Evaluator::tokenize('18+5')
-        );
-    }   
-
-    public function testSimpleSubtractionReturnsResult(): void
-    {
-        $this->assertEquals(
-            "6",
-            Evaluator::evaluate('8 - 2')
-        );
+    /**
+     * @dataProvider invalidExpressionProvider
+     */
+    public function testInvalidExpressions($expr, $expected) {
+        $this->expectException($expected);
+        Evaluator::evaluate($expr);
     }
 
-    public function testSimpleMultipleOperationsReturnsResult(): void
-    {
-        $this->assertEquals(
-            "11",
-            Evaluator::evaluate('8 - 2 + 5')
-        );
-    }
-
-    public function testInvalidOperationsThrowInvalidArgumentException(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        Evaluator::evaluate('+ 8 - 2');
-    }
-
-    public function testInvalidDoubleOperationsThrowInvalidArgumentException(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        Evaluator::evaluate('8 -- 2');
-    }
-
-    public function testInvalidDoubleNumberThrowInvalidArgumentException(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        Evaluator::evaluate('8 8 - 2');
-    }
-
-    public function testSimpleMultiplicationReturnsResult(): void
-    {
-        $this->assertEquals(
-            "45",
-            Evaluator::evaluate('15 * 3')
-        );
-        $this->assertEquals(
-            "135",
-            Evaluator::evaluate('15 * 3 * 3')
-        );
-    }
-
-    public function testSimpleDivisionReturnsResult(): void
-    {
-        $this->assertEquals(
-            "33",
-            Evaluator::evaluate('100 / 3')
-        );
+    public function invalidExpressionProvider() {
+        return [
+            /** [input, expected] */
+            ['+ 8 - 2', InvalidArgumentException::class],
+            ['8 -- 2', InvalidArgumentException::class],
+            ['8 8 - 2', InvalidArgumentException::class],
+        ];
     }
 }
 
