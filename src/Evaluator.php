cc328e0eef508b9aa49b02f82040e5d821d5c819
Jim Connell <jconnell@etsy.com>
2021-05-24 13:26:51 -0400

    leading up to step 3.

--- src/Evaluator.php
+++ src/Evaluator.php
@@ -1,8 +1,26 @@
 <?php declare(strict_types=1);
 
-final class Evaluator 
+final class Evaluator
 {
     public static function evaluate(string $expr) {
+        if ($expr == '') {
+                return '0';
+        }
+        $total = 0;
+        $terms = Evaluator::tokenize($expr);
+        foreach ($terms as $term) {
+                $total += (int) $term;
+        }
+        return $total;
+    }
+
+    public static function tokenize(string $expr) {
+        if ($expr == '') {
+            return [];
+        }
+        $tokens = preg_split('/([*\/+-])\s*|([\d]+)\s*/',
+            $expr, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+        return $tokens;
     }
 }
 
--- tests/EvaluatorTest.php
+++ tests/EvaluatorTest.php
@@ -2,14 +2,55 @@
 use PHPUnit\Framework\TestCase;
 
 final class EvaluatorTest extends TestCase
-{
+{   
     public function testEmptyStringReturnsZero(): void
-    {
+    {           
         $this->assertEquals(
-            "0",
+            "0", 
             Evaluator::evaluate('')
         );
+    }           
+        
+    public function testPlainIntReturnsThatInt(): void
+    {
+        $this->assertEquals(
+            "42",
+            Evaluator::evaluate('42')
+        );
     }
+
+    public function testSimpleAdditionReturnsResult(): void
+    {
+        $this->assertEquals(
+            "10",
+            Evaluator::evaluate('8 + 2')
+        );
+    }
+
+    public function testTokenizeEmptyStringReturnsEmptyArray(): void
+    {           
+        $this->assertEquals(
+            [], 
+            Evaluator::tokenize('')
+        );
+    }   
+
+    public function testTokenizeSingleIntReturnsInt(): void
+    {           
+        $this->assertEquals(
+            ['18'], 
+            Evaluator::tokenize('18')
+        );
+    }
+
+    public function testTokenizeExpressionReturnsExpressionTokens(): void
+    {           
+        $this->assertEquals(
+            ['18', '+', '5'], 
+            Evaluator::tokenize('18+5')
+        );
+    }   
+
 }
 
 ?>
